9ecc59b210568480a780e27fefea333732a35584
Damir Bajramovic <damir.b@debridge.finance>
2026-05-11 09:19:47 +0200

    Remove dev env.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,4 @@
-// export const BASE_URL = "https://api-gaslessb2b.debridge.finance";
-
-// TODO: Remove.
-export const BASE_URL = "https://api-dev.debridge.io/"
+export const BASE_URL = "https://api-gaslessb2b.debridge.finance";
 
 export const V1_BASE = "/v1/gasless";
 export const V1_1_BASE = "/v1.1/gasless";
